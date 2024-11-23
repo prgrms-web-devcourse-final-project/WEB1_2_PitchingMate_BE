e9e92ee7afa43ca4713883b93a7cc44172a66ab8
hongjeZZ <ajyong316@gmail.com>
2024-11-23 20:02:39 +0900

    MATE-10 : [FEAT] 회원 정보 조회 관련 DTO 클래스 구현

---
+++ src/main/java/com/example/mate/domain/member/dto/response/MemberResponse.java
@@ -0,0 +1,18 @@
+package com.example.mate.domain.member.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberResponse {
+
+    private String nickname;
+    private String imageUrl;
+    private String team;
+    private Float manner;
+    private String aboutMe;
+    private Integer followingCount;
+    private Integer followerCount;
+}
