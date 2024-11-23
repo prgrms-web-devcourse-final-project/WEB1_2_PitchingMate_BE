61d392f08a92972f6bb354e4d2a214c2153184df
hongjeZZ <ajyong316@gmail.com>
2024-11-23 20:02:15 +0900

    MATE-10 : [FEAT] 회원 정보 조회 기능 엔드포인트 구현

--- src/main/java/com/example/mate/domain/member/controller/MemberController.java
+++ src/main/java/com/example/mate/domain/member/controller/MemberController.java
@@ -2,7 +2,9 @@ package com.example.mate.domain.member.controller;
 
 import com.example.mate.domain.member.dto.request.JoinRequest;
 import com.example.mate.domain.member.dto.response.JoinResponse;
+import com.example.mate.domain.member.dto.response.MemberResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,5 +35,25 @@ public class MemberController {
                 .build();
 
         return ResponseEntity.ok(joinResponse);
+    }
+
+    /*
+    TODO : 2024/11/23 - 회원 조회
+    1. memberId 회원 정보 조회
+    */
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberResponse> findMemberInfo(@PathVariable Long memberId) {
+
+        MemberResponse memberResponse = MemberResponse.builder()
+                .nickname("삼성빠돌이")
+                .imageUrl("default.jpg")
+                .team("삼성")
+                .manner(0.3f)
+                .aboutMe("삼성을 사랑하는 삼성빠돌이입니다!")
+                .followingCount(10)
+                .followerCount(20)
+                .build();
+
+        return ResponseEntity.ok(memberResponse);
     }
 }
