ae03d2005b73f1d5a6dbe975f0752eb461f0c48b
hongjeZZ <ajyong316@gmail.com>
2024-11-26 12:36:41 +0900

    MATE-14 : [FEAT] GoodsPost 엔티티 구현

---
+++ src/main/java/com/example/mate/domain/goods/entity/GoodsPost.java
@@ -0,0 +1,66 @@
+package com.example.mate.domain.goods.entity;
+
+import com.example.mate.domain.goods.vo.Location;
+import com.example.mate.domain.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "goods_post")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GoodsPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "seller_id", nullable = false)
+    private Member seller;
+
+    @OneToOne
+    @JoinColumn(name = "buyer_id")
+    private Member buyer;
+
+//    @ManyToOne
+//    @JoinColumn(name = "team_id", nullable = false)
+//    private Team team;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Embedded
+    private Location location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private Status status = Status.OPEN;
+}
