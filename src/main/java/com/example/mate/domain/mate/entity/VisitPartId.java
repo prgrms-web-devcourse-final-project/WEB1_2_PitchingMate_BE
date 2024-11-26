cd44e9ee87890c25e414de48d8103480de62c587
sohee <144431880+MisaSohee@users.noreply.github.com>
2024-11-26 15:43:46 +0900

    MATE-16 : [FEAT] 메이트 정보 관련 엔티티 생성 (#12)
    * MATE-16 : [FEAT] MatePost 엔티티 생성
    
    * MATE-16 : [FEAT] MateReview 엔티티 생성
    
    * MATE-16 : [FEAT] Visit 엔티티 생성
    
    * MATE-16 : [FEAT] VisitPart 엔티티 생성
    
    * MATE-16 : [FEAT] VisitPartId 엔티티 생성
    
    * MATE-16 : [FEAT] Team, Match 필드 주석처리

---
+++ src/main/java/com/example/mate/domain/mate/entity/VisitPartId.java
@@ -0,0 +1,15 @@
+package com.example.mate.domain.mate.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VisitPartId implements Serializable {
+    private Long member;
+    private Long visit;
+}
