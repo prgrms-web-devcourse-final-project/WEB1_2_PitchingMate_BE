9d33e80107d43b67869cc0886b2921aa4c72a51d
juchan204 <schultz19@naver.com>
2024-11-28 04:01:06 +0900

    MATE-54 : [FEAT] MatchRepository 경기 전적 조회 메서드 추가 - 1. JPA 사용

--- src/main/java/com/example/mate/domain/match/repository/MatchRepository.java
+++ src/main/java/com/example/mate/domain/match/repository/MatchRepository.java
@@ -1,7 +1,10 @@
 package com.example.mate.domain.match.repository;
 
 import com.example.mate.domain.match.entity.Match;
+import com.example.mate.domain.match.entity.MatchStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,8 @@ import java.util.List;
 public interface MatchRepository extends JpaRepository<Match, Long> {
     List<Match> findTop5ByOrderByMatchTimeDesc();
     List<Match> findTop3ByHomeTeamIdOrAwayTeamIdOrderByMatchTimeDesc(Long homeTeamId, Long awayTeamId);
+    List<Match> findByStatusAndHomeTeamIdOrStatusAndAwayTeamIdOrderByMatchTimeDesc(
+            MatchStatus status1, Long homeTeamId,
+            MatchStatus status2, Long awayTeamId
+    );
 }
