58b02f4a011673011f19db44db6096da175639d4
juchan204 <schultz19@naver.com>
2024-11-28 00:55:38 +0900

    MATE-53 : [FEAT] StadiumResponse 수정 - 1. 빌더 패턴 생성

--- src/main/java/com/example/mate/domain/match/dto/response/StadiumResponse.java
+++ src/main/java/com/example/mate/domain/match/dto/response/StadiumResponse.java
@@ -1,19 +1,36 @@
 package com.example.mate.domain.match.dto.response;
 
+import com.example.mate.domain.constant.StadiumInfo;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StadiumResponse {
     @Getter
-    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class Info {
         private Long id;
         private String stadiumName;
         private String location;
         private String longitude;
         private String latitude;
+
+        @Builder
+        private Info(StadiumInfo.Stadium stadium) {
+            this.id = stadium.id;
+            this.stadiumName = stadium.name;
+            this.location = stadium.location;
+            this.longitude = stadium.longitude;
+            this.latitude = stadium.latitude;
+        }
+
+        public static Info from(StadiumInfo.Stadium stadium) {
+            return Info.builder()
+                    .stadium(stadium)
+                    .build();
+        }
     }
 }
