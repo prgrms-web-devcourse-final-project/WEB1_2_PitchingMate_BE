46f957d76dbb96e31a42f717c599c031fc2f54ff
jooinjoo <jooinjoo0@gmail.com>
2024-11-27 22:04:48 +0900

    MATE-18 : [FEAT] 잘못된 Validation 값에 대한 예외 코드 및 핸들러 추가

--- src/main/java/com/example/mate/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/mate/common/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.mate.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     // CustomException 처리
     @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
         log.error("CustomException: {}", e.getMessage());
         ErrorCode errorCode = e.getErrorCode();
 
@@ -21,9 +22,22 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.error(errorCode));
     }
 
+    // 유효성 검사 오류가 발생한 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Validation failed: {}", e.getMessage(), e);
+        
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(
+                        "유효성 검사 오류가 발생했습니다.",
+                        HttpStatus.BAD_REQUEST.value()
+                ));
+    }
+
     // 모든 예외 타입 처리
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
         log.error("Unexpected error: {}", e.getMessage(), e);
 
         return ResponseEntity
