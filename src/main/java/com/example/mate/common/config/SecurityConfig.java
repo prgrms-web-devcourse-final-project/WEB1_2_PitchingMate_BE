d68062821e909fb4355e7cef2742423b9b90f3e6
jooinjoo <jooinjoo0@gmail.com>
2024-11-28 14:57:54 +0900

    MATE-59 : [FEAT] JWT 필터 체인 설정 추가
    - JWT 필터를 UsernamePasswordAuthenticationFilter 전에 추가

--- src/main/java/com/example/mate/common/config/SecurityConfig.java
+++ src/main/java/com/example/mate/common/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.example.mate.common.config;
 
+import com.example.mate.common.security.filter.JwtCheckFilter;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtCheckFilter jwtCheckFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,6 +34,8 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtCheckFilter,
+                        UsernamePasswordAuthenticationFilter.class)   // JWT 필터를 UsernamePasswordAuthenticationFilter 전에 추가
                 .authorizeHttpRequests(req ->
                         req.anyRequest().permitAll())
                 .build();
