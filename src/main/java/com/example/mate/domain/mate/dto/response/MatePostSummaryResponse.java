3da07f0b657d59f11098ab85030dcaa806d34550
MisaSohee <sohee5951@naver.com>
2024-11-28 18:14:31 +0900

    MATE-25 : [FEAT] 메인 페이지의 메이트 게시글 조회 반환 DTO 구현

--- src/main/java/com/example/mate/domain/mate/dto/response/MatePostSummaryResponse.java
+++ src/main/java/com/example/mate/domain/mate/dto/response/MatePostSummaryResponse.java
@@ -1,7 +1,10 @@
 package com.example.mate.domain.mate.dto.response;
 
-import com.example.mate.domain.mate.entity.Age;
 import com.example.mate.domain.constant.Gender;
+import com.example.mate.domain.constant.TeamInfo;
+import com.example.mate.domain.match.entity.Match;
+import com.example.mate.domain.mate.entity.Age;
+import com.example.mate.domain.mate.entity.MatePost;
 import com.example.mate.domain.mate.entity.Status;
 import com.example.mate.domain.mate.entity.TransportType;
 import lombok.Builder;
@@ -23,4 +26,33 @@ public class MatePostSummaryResponse {
     private Age age;
     private Gender gender;
     private TransportType transportType;
+
+    public static MatePostSummaryResponse from(MatePost post) {
+        return MatePostSummaryResponse.builder()
+                .imageUrl(post.getImageUrl())
+                .title(post.getTitle())
+                .status(post.getStatus())
+                .rivalTeamName(getRivalTeamName(post))
+                .rivalMatchTime(post.getMatch().getMatchTime())
+                .location(post.getMatch().getStadium().name)
+                .maxParticipants(post.getMaxParticipants())
+                .age(post.getAge())
+                .gender(post.getGender())
+                .transportType(post.getTransport())
+                .build();
+    }
+
+    private static String getRivalTeamName(MatePost post) {
+        Match match = post.getMatch();
+        Long postTeamId = post.getTeamId(); // 게시글 작성자가 선택한 팀
+
+        // 게시글 작성자가 선택한 팀이 홈팀인 경우 원정팀이 상대팀
+        if (postTeamId.equals(match.getHomeTeamId())) {
+            return TeamInfo.getById(match.getAwayTeamId()).shortName;
+        }
+        // 게시글 작성자가 선택한 팀이 원정팀인 경우 홈팀이 상대팀
+        else {
+            return TeamInfo.getById(match.getHomeTeamId()).shortName;
+        }
+    }
 }
