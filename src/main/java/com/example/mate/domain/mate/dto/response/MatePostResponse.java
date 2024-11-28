8af424b44b5c77e1076da8929ec3bb874dc348e8
MisaSohee <sohee5951@naver.com>
2024-11-28 12:59:46 +0900

    MATE-25 : [FEAT] 메이트 게시글 작성 반환 DTO에 엔티티 변환 메서드 추가

--- src/main/java/com/example/mate/domain/mate/dto/response/MatePostResponse.java
+++ src/main/java/com/example/mate/domain/mate/dto/response/MatePostResponse.java
@@ -1,5 +1,6 @@
 package com.example.mate.domain.mate.dto.response;
 
+import com.example.mate.domain.mate.entity.MatePost;
 import com.example.mate.domain.mate.entity.Status;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,4 +10,8 @@ import lombok.Getter;
 public class MatePostResponse {
     private Long id;
     private Status status;
+
+    public static MatePostResponse from(MatePost matePost) {
+        return MatePostResponse.builder().id(matePost.getId()).status(matePost.getStatus()).build();
+    }
 }
