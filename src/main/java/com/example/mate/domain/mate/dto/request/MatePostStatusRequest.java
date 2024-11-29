c87245e1d7b8bfc16ffb5d82b1b69362e59ccd24
MisaSohee <sohee5951@naver.com>
2024-11-30 00:42:22 +0900

    MATE-28 : [FEAT] 메이트 게시물 상태변경 요청 DTO 구현

--- src/main/java/com/example/mate/domain/mate/dto/request/MatePostStatusRequest.java
+++ src/main/java/com/example/mate/domain/mate/dto/request/MatePostStatusRequest.java
@@ -1,9 +1,17 @@
 package com.example.mate.domain.mate.dto.request;
 
+import com.example.mate.common.utils.validator.ValidEnum;
 import com.example.mate.domain.mate.entity.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MatePostStatusRequest {
+    @ValidEnum(message = "모집 상태의 입력 값이 잘못되었습니다.", enumClass = Status.class)
     private Status status;
 }
