b657875a7db95d818cf04ad956a7305bd0f281b2
jooinjoo <jooinjoo0@gmail.com>
2024-11-30 07:14:12 +0900

    MATE-37 : [FEAT] 내 프로필/다른 유저 프로필 조회 서비스, 컨트롤러 구현 및 예외 처리

--- src/main/java/com/example/mate/domain/member/controller/MemberController.java
+++ src/main/java/com/example/mate/domain/member/controller/MemberController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,27 +46,11 @@ public class MemberController {
         return ResponseEntity.ok(ApiResponse.success(memberService.join(joinRequest)));
     }
 
-    /*
-    TODO : 2024/11/23 - 내 프로필 조회
-    1. JwtToken 을 통해 사용자 정보 조회
-    */
+    // TODO : 2024/11/29 - 내 프로필 조회 : 추후 @AuthenticationPrincipal Long memberId 받음
+    @Operation(summary = "내 프로필 조회")
     @GetMapping("/me")
-    public ResponseEntity<MyProfileResponse> findMemberInfo() {
-        MyProfileResponse myProfileResponse = MyProfileResponse.builder()
-                .nickname("삼성빠돌이")
-                .imageUrl("default.jpg")
-                .teamName("삼성")
-                .manner(0.3f)
-                .aboutMe("삼성을 사랑하는 삼성빠돌이입니다!")
-                .followingCount(10)
-                .followerCount(20)
-                .reviewsCount(10)
-                .goodsSoldCount(20)
-                .goodsBoughtCount(10)
-                .visitsCount(20)
-                .build();
-
-        return ResponseEntity.ok(myProfileResponse);
+    public ResponseEntity<ApiResponse<MyProfileResponse>> findMyInfo(@RequestParam Long memberId) {
+        return ResponseEntity.ok(ApiResponse.success(memberService.getMyProfile(memberId)));
     }
 
     @Operation(summary = "다른 회원 프로필 조회")
