3628176eeaca3bdbaba1a8204cd2c780fa1ad576
jooinjoo <jooinjoo0@gmail.com>
2024-11-29 21:52:13 +0900

    MATE-61 : [FEAT] 회원 가입 요청/응답 DTO 수정

--- src/main/java/com/example/mate/domain/member/dto/response/JoinResponse.java
+++ src/main/java/com/example/mate/domain/member/dto/response/JoinResponse.java
@@ -1,5 +1,7 @@
 package com.example.mate.domain.member.dto.response;
 
+import com.example.mate.domain.constant.TeamInfo;
+import com.example.mate.domain.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,4 +18,15 @@ public class JoinResponse {
     private Integer age;
     private String gender;
     private String team;
+
+    public static JoinResponse from(Member member) {
+        return JoinResponse.builder()
+                .name(member.getName())
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .age(member.getAge())
+                .gender(member.getGender().getValue())
+                .team(TeamInfo.getById(member.getTeamId()).shortName)
+                .build();
+    }
 }
