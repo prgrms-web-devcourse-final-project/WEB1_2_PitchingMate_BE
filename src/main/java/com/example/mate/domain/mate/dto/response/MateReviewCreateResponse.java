9b5584823a389943c5717f4866b35609eb2db4d9
MisaSohee <sohee5951@naver.com>
2024-12-01 16:33:26 +0900

    MATE-31 : [FEAT] 직관 후기 반환 DTO 구현

--- src/main/java/com/example/mate/domain/mate/dto/response/MateReviewCreateResponse.java
+++ src/main/java/com/example/mate/domain/mate/dto/response/MateReviewCreateResponse.java
@@ -1,10 +1,27 @@
 package com.example.mate.domain.mate.dto.response;
 
+import com.example.mate.domain.mate.entity.MateReview;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class MateReviewCreateResponse {
-    private Long id;
+    private Long reviewId;
+    private Long reviewerId;
+    private Long revieweeId;
+    private String revieweeName;
+    private String content;
+    private String rating;
+
+    public static MateReviewCreateResponse from(MateReview review) {
+        return MateReviewCreateResponse.builder()
+                .reviewId(review.getId())
+                .reviewerId(review.getReviewer().getId())
+                .revieweeId(review.getReviewee().getId())
+                .revieweeName(review.getReviewee().getName())
+                .content(review.getReviewContent())
+                .rating(review.getRating().getValue())
+                .build();
+    }
 }
