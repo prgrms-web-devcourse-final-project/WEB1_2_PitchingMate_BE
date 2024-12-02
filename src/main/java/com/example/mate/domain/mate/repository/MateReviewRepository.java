a59368694c9ebb0d6f9666af4f7d22444f04910c
jooinjoo <jooinjoo0@gmail.com>
2024-12-03 00:31:39 +0900

    MATE-42 : [FEAT] 직관, 리뷰어, 리뷰 대상을 통해 해당 직관에서 특정 회원에 남긴 리뷰 조회

--- src/main/java/com/example/mate/domain/mate/repository/MateReviewRepository.java
+++ src/main/java/com/example/mate/domain/mate/repository/MateReviewRepository.java
@@ -1,9 +1,13 @@
 package com.example.mate.domain.mate.repository;
 
 import com.example.mate.domain.mate.entity.MateReview;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MateReviewRepository extends JpaRepository<MateReview, Long> {
 
     int countByRevieweeId(Long revieweeId);
+
+    Optional<MateReview> findMateReviewByVisitIdAndReviewerIdAndRevieweeId(Long visitId, Long reviewerId,
+                                                                           Long revieweeId);
 }
