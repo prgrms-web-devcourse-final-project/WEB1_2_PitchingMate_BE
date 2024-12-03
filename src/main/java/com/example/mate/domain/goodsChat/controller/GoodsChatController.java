5ff12f0bf72816ac32875cac76572e1015957e74
hongjeZZ <ajyong316@gmail.com>
2024-12-03 14:24:35 +0900

    MATE-77: [FEAT] 굿즈거래 채팅방 채팅내역 조회 기능 구현

--- src/main/java/com/example/mate/domain/goodsChat/controller/GoodsChatController.java
+++ src/main/java/com/example/mate/domain/goodsChat/controller/GoodsChatController.java
@@ -1,10 +1,16 @@
 package com.example.mate.domain.goodsChat.controller;
 
 import com.example.mate.common.response.ApiResponse;
+import com.example.mate.common.response.PageResponse;
+import com.example.mate.domain.goodsChat.dto.response.GoodsChatMsgResponse;
 import com.example.mate.domain.goodsChat.dto.response.GoodsChatRoomResponse;
 import com.example.mate.domain.goodsChat.service.GoodsChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,9 +29,23 @@ public class GoodsChatController {
     "/api/goods/chat" 로 변경 예정
     */
     @PostMapping
-    public ResponseEntity<ApiResponse<GoodsChatRoomResponse>> createGoodsChatRoom(@RequestParam Long buyerId, @RequestParam Long goodsPostId) {
+    public ResponseEntity<ApiResponse<GoodsChatRoomResponse>> createGoodsChatRoom(@RequestParam Long buyerId,
+                                                                                  @RequestParam Long goodsPostId) {
         GoodsChatRoomResponse response = goodsChatService.getOrCreateGoodsChatRoom(buyerId, goodsPostId);
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
 
+    /*
+    굿즈거래 채팅방 페이지 - 채팅 내역 조회
+    TODO: @RequestParam Long memberId -> @AuthenticationPrincipal 로 변경
+    */
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<ApiResponse<PageResponse<GoodsChatMsgResponse>>> getGoodsChatRoomMessages(
+            @PathVariable Long chatRoomId,
+            @RequestParam Long memberId,
+            @PageableDefault Pageable pageable
+    ) {
+        PageResponse<GoodsChatMsgResponse> response = goodsChatService.getMessagesForChatRoom(chatRoomId, memberId, pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
