cb83c55881f043e4daf4c1bf5a55bc6cae0d52cc
MisaSohee <sohee5951@naver.com>
2024-12-03 12:02:25 +0900

    MATE-74 : [FEAT] MateChatRoomMemberm 클래스 구현

---
+++ src/main/java/com/example/mate/domain/mateChat/entity/MateChatRoomMember.java
@@ -0,0 +1,39 @@
+package com.example.mate.domain.mateChat.entity;
+
+import com.example.mate.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "chat_room_member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class MateChatRoomMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private MateChatRoom mateChatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "is_active", nullable = false)
+    @Builder.Default
+    private Boolean isActive = true;
+
+    private MateChatRoomMember(MateChatRoom mateChatRoom, Member member) {
+        this.mateChatRoom = mateChatRoom;
+        this.member = member;
+    }
+
+    public void deactivate() {
+        this.isActive = false;
+        this.mateChatRoom.decrementCurrentMembers();
+    }
+}
