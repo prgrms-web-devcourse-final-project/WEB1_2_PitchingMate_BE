8d5f5d7df26fe68df2c75ccc3844299839c58a81
juchan204 <schultz19@naver.com>
2024-12-06 03:41:29 +0900

    MATE-100 : [FIX] 스크립트 수정 -1. 현재 경로가 catchmi-server/catchmi-server로 중복- 이를 해결

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop catchmi-server || true
 docker rm catchmi-server || true
-docker pull 533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server/catchmi-server:latest
-docker run -d --name catchmi-server -p 8080:8080 533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server/catchmi-server:latest
+docker pull 533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server:latest
+docker run -d --name catchmi-server -p 8080:8080 533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
