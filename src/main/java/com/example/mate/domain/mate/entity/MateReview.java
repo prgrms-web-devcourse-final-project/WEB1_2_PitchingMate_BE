90119ea9945cc463db55c3a75ac3d61cacdfb355
jooinjoo <jooinjoo0@gmail.com>
2024-12-05 17:14:31 +0900

    MATE-93 : [CHORE] 사소한 import 정리

--- src/main/java/com/example/mate/domain/mate/entity/MateReview.java
+++ src/main/java/com/example/mate/domain/mate/entity/MateReview.java
@@ -3,8 +3,22 @@ package com.example.mate.domain.mate.entity;
 import com.example.mate.common.BaseTimeEntity;
 import com.example.mate.domain.constant.Rating;
 import com.example.mate.domain.member.entity.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "mate_review")
