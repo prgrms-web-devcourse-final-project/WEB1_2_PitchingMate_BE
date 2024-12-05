012a70068f494c800b04049e6cdb29855a581ad7
juchan204 <schultz19@naver.com>
2024-12-06 03:59:38 +0900

    MATE-100 : [FIX] 도커에 환경변수 전달

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#!/bin/bash
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop catchmi-server || true
 docker rm catchmi-server || true
 docker pull 533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server:latest
-docker run -d --name catchmi-server -p 8080:8080 533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server:latest
+docker run -d --name catchmi-server \
+  -p 8080:8080 \
+  -e JWT_SECRET_KEY="${JWT_SECRET_KEY}" \
+  -e NAVER_CLIENT_ID="${NAVER_CLIENT_ID}" \
+  -e NAVER_REDIRECT_URI="${NAVER_REDIRECT_URI}" \
+  -e NAVER_CLIENT_SECRET="${NAVER_CLIENT_SECRET}" \
+  -e OPENWEATHER_API_KEY="${OPENWEATHER_API_KEY}" \
+  533267244952.dkr.ecr.ap-northeast-2.amazonaws.com/catchmi-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
