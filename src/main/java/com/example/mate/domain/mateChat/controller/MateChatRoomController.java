a3a1af66c910aeb94f84d1f326ba34ec40d5f496
MisaSohee <sohee5951@naver.com>
2024-12-06 11:30:48 +0900

    MATE-79 : [FEAT] 메이트 채팅방 컨트롤러 구현

--- src/main/java/com/example/mate/domain/mateChat/controller/MateChatRoomController.java
+++ src/main/java/com/example/mate/domain/mateChat/controller/MateChatRoomController.java
@@ -5,7 +5,6 @@ import com.example.mate.common.response.PageResponse;
 import com.example.mate.domain.mateChat.dto.response.MateChatMessageResponse;
 import com.example.mate.domain.mateChat.dto.response.MateChatRoomListResponse;
 import com.example.mate.domain.mateChat.dto.response.MateChatRoomResponse;
-import com.example.mate.domain.mateChat.service.MateChatMessageService;
 import com.example.mate.domain.mateChat.service.MateChatRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -23,28 +22,38 @@ import org.springframework.web.bind.annotation.*;
 public class MateChatRoomController {
     private final MateChatRoomService chatRoomService;
 
-    @PostMapping("/rooms/{matePostId}")
-    @Operation(summary = "채팅방 생성/입장", description = "")
-    public ResponseEntity<ApiResponse<MateChatRoomResponse>> createOrJoinChatRoom(
+    @PostMapping("/post/{matePostId}")
+    @Operation(summary = "메이트 게시글 -> 채팅방 생성/입장", description = "메이트 게시글 페이지에서 채팅방으로 입장")
+    public ResponseEntity<ApiResponse<MateChatRoomResponse>> createOrJoinChatRoomFromPost(
             @Parameter(description = "메이트 게시글 ID") @PathVariable Long matePostId,
-            @Parameter(description = "회원 ID (삭제 예정)") @RequestParam Long memberId  // 추후 @AuthenticationPrincipal로 대체
+            @Parameter(description = "회원 ID (삭제 예정)") @RequestParam Long memberId
     ) {
-        MateChatRoomResponse response = chatRoomService.createOrJoinChatRoom(matePostId, memberId);
+        MateChatRoomResponse response = chatRoomService.createOrJoinChatRoomFromPost(matePostId, memberId);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @GetMapping("/rooms/{roomId}/messages")
-    @Operation(summary = "채팅 메시지 조회", description = "채팅 목록 페이지에서 특정 채팅방 입장 시, 메시지 내역을 조회합니다.")
+    @PostMapping("/{chatroomId}/join")
+    @Operation(summary = "채팅방 목록 -> 채팅방 입장", description = "채팅 목록 페이지에서 채팅방으로 입장")
+    public ResponseEntity<ApiResponse<MateChatRoomResponse>> joinExistingChatRoom(
+            @Parameter(description = "채팅방 ID") @PathVariable Long roomId,
+            @Parameter(description = "회원 ID (삭제 예정)") @RequestParam Long memberId
+    ) {
+        MateChatRoomResponse response = chatRoomService.joinExistingChatRoom(roomId, memberId);
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping("/{chatroomId}/members/{memberId}/messages")
+    @Operation(summary = "채팅방 메세지 조회", description = "채팅 목록 페이지 -> 채팅방 입장 시, 메시지 내역을 조회합니다.")
     public ResponseEntity<ApiResponse<PageResponse<MateChatMessageResponse>>> getChatMessages(
             @Parameter(description = "채팅방 ID") @PathVariable Long roomId,
+            @PathVariable Long memberId,
             @Parameter(description = "페이지 정보") @PageableDefault Pageable pageable
     ) {
-        PageResponse<MateChatMessageResponse> messages = chatRoomService.getChatMessages(roomId, pageable);
+        PageResponse<MateChatMessageResponse> messages = chatRoomService.getChatMessages(roomId, memberId, pageable);
         return ResponseEntity.ok(ApiResponse.success(messages));
     }
 
-    // 채팅 페이지를 눌렀을 때 채팅 목록을 반환하기 위해서 호출됨.
-    @GetMapping("/rooms/me")
+    @GetMapping("/me")
     @Operation(summary = "내 채팅방 목록 조회", description = "사용자의 채팅방 목록을 조회합니다.")
     public ResponseEntity<ApiResponse<PageResponse<MateChatRoomListResponse>>> getMyChatRooms(
             @Parameter(description = "회원 ID (삭제 예정)") @RequestParam Long memberId,  // 추후 @AuthenticationPrincipal로 대체
@@ -54,8 +63,7 @@ public class MateChatRoomController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    // 채팅방 나갈 때 호출되고, 이후 handleLeave 메서드 호출로 인해 퇴장 메세지 전송됨
-    @DeleteMapping("/rooms/{roomId}/leave")
+    @DeleteMapping("{chatroomId}/leave")
     @Operation(summary = "채팅방 나가기", description = "채팅방에서 퇴장합니다.")
     public ResponseEntity<Void> leaveChatRoom(
             @Parameter(description = "채팅방 ID") @PathVariable Long roomId,
