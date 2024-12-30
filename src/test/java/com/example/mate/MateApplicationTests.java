f1ee4a6f0a7ad41bd59d6c697ca9f0abc5b0b7ab
juchan204 <schultz19@naver.com>
2024-12-30 15:29:29 +0900

    MATE-129 : [TEST] 환경변수 테스트용 더미값 설정

--- src/test/java/com/example/mate/MateApplicationTests.java
+++ src/test/java/com/example/mate/MateApplicationTests.java
@@ -10,7 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
         "oauth.naver.client-id=test_client_id",
         "oauth.naver.redirect-uri=http://localhost:8080/callback",
         "oauth.naver.client-secret=test_client_secret",
-        "openweather.api.key=test_api_key"
+        "openweather.api.key=test_api_key",
+        "cloud.aws.s3.bucket=test-bucket-name",
+        "cloud.aws.credentials.access_key=test-access-key",
+        "cloud.aws.credentials.secret_key=test-secret-key",
+        "cloud.aws.region.static=ap-northeast-2"
 })
 @EnableConfigurationProperties(OAuthConfig.class)
 class MateApplicationTests {
