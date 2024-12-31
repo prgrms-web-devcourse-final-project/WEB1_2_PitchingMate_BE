5c1b160970ea073f5ba5e67298688141dd22efd0
juchan204 <schultz19@naver.com>
2024-12-31 15:05:36 +0900

    MATE-129 : [TEST] properties 설정 임의로 제거

--- src/test/java/com/example/mate/MateApplicationTests.java
+++ src/test/java/com/example/mate/MateApplicationTests.java
@@ -5,17 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "jwt.secret_key=testsecretkey12345678901234567890",
-        "oauth.naver.client-id=test_client_id",
-        "oauth.naver.redirect-uri=http://localhost:8080/callback",
-        "oauth.naver.client-secret=test_client_secret",
-        "openweather.api.key=test_api_key",
-        "cloud.aws.s3.bucket=test-bucket-name",
-        "cloud.aws.credentials.access_key=test-access-key",
-        "cloud.aws.credentials.secret_key=test-secret-key",
-        "cloud.aws.region.static=ap-northeast-2"
-})
+@SpringBootTest
 @EnableConfigurationProperties(OAuthConfig.class)
 class MateApplicationTests {
 
