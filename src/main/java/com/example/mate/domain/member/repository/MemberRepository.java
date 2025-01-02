f38da0fa25cd47abe13c6d921605e82892f7d26a
jooinjoo <jooinjoo0@gmail.com>
2025-01-02 16:32:53 +0900

    MATE-134 : [REFACTOR] MemberRepository의 조회 메서드를 전부 탈퇴하지 않은 회원 기준으로 변경

--- src/main/java/com/example/mate/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/mate/domain/member/repository/MemberRepository.java
@@ -1,16 +1,21 @@
 package com.example.mate.domain.member.repository;
 
 import com.example.mate.domain.member.entity.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Member m WHERE m.nickname = :nickname AND m.isDeleted = false")
     boolean existsByNickname(String nickname);
 
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Member m WHERE m.email = :email AND m.isDeleted = false")
     boolean existsByEmail(String email);
 
+    @Query("SELECT m FROM Member m WHERE m.email = :email AND m.isDeleted = false")
     Optional<Member> findByEmail(String email);
 }
