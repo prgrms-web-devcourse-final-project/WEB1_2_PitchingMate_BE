c694e51dca6eb8357cd82cb5d69649052da5752f
juchan204 <schultz19@naver.com>
2025-01-08 15:08:10 +0900

    MATE-146 : [FIX] 배너 경기 데이터 조회 SQL 수정 - 1. Native Query 대신 JPQL Pageable 사용

--- src/main/java/com/example/mate/domain/match/service/MatchService.java
+++ src/main/java/com/example/mate/domain/match/service/MatchService.java
@@ -8,6 +8,7 @@ import com.example.mate.domain.match.entity.Match;
 import com.example.mate.domain.match.repository.MatchRepository;
 import com.example.mate.domain.match.util.WeekCalculator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
@@ -27,16 +28,15 @@ public class MatchService {
     private static final int WEEKS_TO_FETCH = 4;
 
     public List<MatchResponse> getMainBannerMatches() {
-        return matchRepository.findMainBannerMatches(LocalDateTime.now())
+        return matchRepository.findMainBannerMatches(LocalDateTime.now(), PageRequest.of(0, 5))
                 .stream()
                 .map(match -> MatchResponse.from(match, null))
                 .collect(Collectors.toList());
     }
 
-
     public List<MatchResponse> getTeamMatches(Long teamId) {
         TeamInfo.getById(teamId);
-        return matchRepository.findTop3TeamMatchesAfterNow(teamId, LocalDateTime.now())
+        return matchRepository.findTop3TeamMatchesAfterNow(teamId, LocalDateTime.now(), PageRequest.of(0, 3))
                 .stream()
                 .map(match -> MatchResponse.from(match, teamId))
                 .collect(Collectors.toList());
