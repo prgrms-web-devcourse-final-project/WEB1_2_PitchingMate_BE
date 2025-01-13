13f16e8a065b73cffb2b50b05dc192ebb3919887
hongjeZZ <ajyong316@gmail.com>
2025-01-13 14:21:16 +0900

    MATE-150 : [TEST] 인메모리 MongoDB 사용을 위한 설정 클래스 구현
    - 가짜 MongoDB 서버 설정 정보를 담는 MongoTestServerConfig 클래스 구현
    - MongoTestServerConfig 를 Import 하는 EnableMongoTestServer 어노테이션 구현

---
+++ src/test/java/com/example/mate/config/mongoConfig/MongoTestServerConfig.java
@@ -0,0 +1,30 @@
+package com.example.mate.config.mongoConfig;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+public class MongoTestServerConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory) {
+        return new MongoTemplate(mongoDbFactory);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDbFactory(MongoServer mongoServer) {
+        String connectionString = mongoServer.getConnectionString();
+        return new SimpleMongoClientDatabaseFactory(connectionString + "/test");
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public MongoServer mongoServer() {
+        MongoServer mongoServer = new MongoServer(new MemoryBackend());
+        mongoServer.bind();
+
+        return mongoServer;
+    }
+}
