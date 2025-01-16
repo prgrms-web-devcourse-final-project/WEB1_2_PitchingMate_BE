b7b7ebeec444d5aee25520be0b9ca299c4f5ec27
jooinjoo <jooinjoo0@gmail.com>
2025-01-16 15:01:59 +0900

    MATE-154 : [FEAT] 알림 유형에 따른 페이징 조회 서비스 및 레포지토리 구현

--- src/main/java/com/example/mate/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/mate/domain/notification/repository/NotificationRepository.java
@@ -1,9 +1,41 @@
 package com.example.mate.domain.notification.repository;
 
 import com.example.mate.domain.notification.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Query("""
+            SELECT n
+            FROM Notification n
+            WHERE n.receiver.id = :receiverId
+            AND (n.notificationType = 'MATE_CLOSED' OR n.notificationType = 'MATE_COMPLETE')
+            ORDER BY n.id DESC
+            """)
+    Page<Notification> findMateNotificationsByReceiverId(@Param("receiverId") Long receiverId,
+                                                         Pageable pageable);
+
+    @Query("""
+            SELECT n
+            FROM Notification n
+            WHERE n.receiver.id = :receiverId
+            AND n.notificationType = 'GOODS_CLOSED'
+            ORDER BY n.id DESC
+            """)
+    Page<Notification> findGoodsNotificationsByReceiverId(@Param("receiverId") Long receiverId,
+                                                          Pageable pageable);
+
+    @Query("""
+            SELECT n
+            FROM Notification n
+            WHERE n.receiver.id = :receiverId
+            ORDER BY n.id DESC
+            """)
+    Page<Notification> findNotificationsByReceiverId(@Param("receiverId") Long receiverId, Pageable pageable);
 }
