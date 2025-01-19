6aa98b3d66ea58e01caeee243cde213e26297a78
MisaSohee <sohee5951@naver.com>
2025-01-19 22:02:57 +0900

    MATE-151 : [FEAT] 불필요한 헬퍼 메서드 제거

--- src/main/java/com/example/mate/domain/mateChat/event/MateChatEvent.java
+++ src/main/java/com/example/mate/domain/mateChat/event/MateChatEvent.java
@@ -1,24 +1,10 @@
 package com.example.mate.domain.mateChat.event;
 
-import com.example.mate.domain.mateChat.dto.request.MateChatMessageRequest;
 import com.example.mate.domain.mateChat.message.MessageType;
 import com.example.mate.domain.member.entity.Member;
 
 public record MateChatEvent(Long chatRoomId, Member member, MessageType type) {
     public static MateChatEvent from(Long chatRoomId, Member member, MessageType type) {
         return new MateChatEvent(chatRoomId, member, type);
-    }
-
-    public MateChatMessageRequest toMessageRequest() {
-        String content = type == MessageType.ENTER ?
-                member.getNickname() + "님이 들어왔습니다." :
-                member.getNickname() + "님이 나갔습니다.";
-
-        return MateChatMessageRequest.builder()
-                .roomId(chatRoomId)
-                .senderId(member.getId())
-                .type(type.name())
-                .message(content)
-                .build();
     }
 }
