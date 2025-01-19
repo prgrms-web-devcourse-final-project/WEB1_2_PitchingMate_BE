3d7276914bc3baa0b9b619ff06e25a70fcc52772
MisaSohee <sohee5951@naver.com>
2025-01-19 21:36:45 +0900

    MATE-151 : [FEAT] 메이트 메세지의 MongoRepository 생성

--- src/main/java/com/example/mate/domain/mateChat/repository/MateChatMessageRepository.java
+++ src/main/java/com/example/mate/domain/mateChat/repository/MateChatMessageRepository.java
@@ -1,21 +1,19 @@
 package com.example.mate.domain.mateChat.repository;
 
-import com.example.mate.domain.mateChat.entity.MateChatMessage;
+import com.example.mate.domain.mateChat.document.MateChatMessage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.time.LocalDateTime;
 
-public interface MateChatMessageRepository extends JpaRepository<MateChatMessage, Long> {
-
-    @Query("SELECT m FROM MateChatMessage m " +
-            "WHERE m.mateChatRoom.id = :roomId " +
-            "AND m.createdAt >= :enterTime " +
-            "ORDER BY m.createdAt DESC ")
-    Page<MateChatMessage> findByChatRoomIdAndCreatedAtAfterOrderByCreatedAtDesc(@Param("roomId") Long roomId,
-                                                                                @Param("enterTime") LocalDateTime enterTime,
-                                                                                Pageable pageable);
+public interface MateChatMessageRepository extends MongoRepository<MateChatMessage, String> {
+    // 채팅방의 특정 시간 이후 메시지 조회 (페이징)
+    @Query("{ 'roomId': ?0, 'sendTime': { $gt: ?1 } }")
+    Page<MateChatMessage> findByRoomIdAndSendTimeAfter(
+            Long roomId,
+            LocalDateTime enterTime,
+            Pageable pageable
+    );
 }
