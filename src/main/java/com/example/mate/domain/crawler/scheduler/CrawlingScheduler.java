4e52200c42e1de9631c85236ad2db0dfd880130f
juchan204 <schultz19@naver.com>
2025-01-20 23:22:46 +0900

    MATE-148 : [FEAT] 기존 스케쥴러를 사용한 크롤링 주석처리

--- src/main/java/com/example/mate/domain/crawler/scheduler/CrawlingScheduler.java
+++ src/main/java/com/example/mate/domain/crawler/scheduler/CrawlingScheduler.java
@@ -13,36 +13,36 @@ import org.springframework.stereotype.Component;
 public class CrawlingScheduler {
     private final CrawlingService crawlingService;
 
-    // 매시 30분
-    @Scheduled(cron = "0 30 * * * *", zone = "Asia/Seoul")
-    public void morningUpdate() {
-        log.info("Starting morning schedule update");
-        try {
-            // 현재 경기 크롤링
-            crawlingService.crawlAllCurrentMatches();
-            // 팀 순위 크롤링
-            crawlingService.crawlTeamRankings();
-        } catch (CustomException e) {
-            log.error("Morning update failed: {}", e.getErrorCode().getMessage(), e);
-        } catch (Exception e) {
-            log.error("Morning update failed with unexpected error", e);
-        }
-    }
-
-    @Scheduled(cron = "0 0 22 * * *", zone = "Asia/Seoul")
-    public void eveningUpdate() {
-        log.info("Starting evening schedule update");
-        try {
-            // 현재 경기 크롤링
-            crawlingService.crawlAllCurrentMatches();
-            // 팀 순위 크롤링
-            crawlingService.crawlTeamRankings();
-        } catch (CustomException e) {
-            log.error("Evening update failed: {}", e.getErrorCode().getMessage(), e);
-        } catch (Exception e) {
-            log.error("Evening update failed with unexpected error", e);
-        }
-    }
+//    // 매시 30분
+//    @Scheduled(cron = "0 0 9 * * *", zone = "Asia/Seoul")
+//    public void morningUpdate() {
+//        log.info("Starting morning schedule update");
+//        try {
+//            // 현재 경기 크롤링
+//            crawlingService.crawlAllCurrentMatches();
+//            // 팀 순위 크롤링
+//            crawlingService.crawlTeamRankings();
+//        } catch (CustomException e) {
+//            log.error("Morning update failed: {}", e.getErrorCode().getMessage(), e);
+//        } catch (Exception e) {
+//            log.error("Morning update failed with unexpected error", e);
+//        }
+//    }
+//
+//    @Scheduled(cron = "0 0 22 * * *", zone = "Asia/Seoul")
+//    public void eveningUpdate() {
+//        log.info("Starting evening schedule update");
+//        try {
+//            // 현재 경기 크롤링
+//            crawlingService.crawlAllCurrentMatches();
+//            // 팀 순위 크롤링
+//            crawlingService.crawlTeamRankings();
+//        } catch (CustomException e) {
+//            log.error("Evening update failed: {}", e.getErrorCode().getMessage(), e);
+//        } catch (Exception e) {
+//            log.error("Evening update failed with unexpected error", e);
+//        }
+//    }
 
 //    // 매 분 실행 자동화 테스트 (현재 off-season )
 //    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
