b979dfafb1935bbc51f797b697e10da9c0333eae
MisaSohee <sohee5951@naver.com>
2025-01-28 13:35:56 +0900

    MATE-158 : [FEAT] 메세지 도큐먼트 인덱스 및 필드 적용

--- src/main/java/com/example/mate/domain/mateChat/document/MateChatMessage.java
+++ src/main/java/com/example/mate/domain/mateChat/document/MateChatMessage.java
@@ -4,7 +4,10 @@ import com.example.mate.common.BaseTimeEntity;
 import com.example.mate.domain.mateChat.message.MessageType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
@@ -13,13 +16,27 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@CompoundIndexes({
+        @CompoundIndex(name = "idx_chat_room_id_sent_at", def = "{ 'chat_room_id': 1, 'sent_at': -1 }")
+}
+)
 public class MateChatMessage extends BaseTimeEntity {
     @Id
     private String id;
 
+    @Field(name = "room_id")
     private Long roomId;
+
+    @Field(name = "sender_id")
     private Long senderId;
-    private MessageType type;
+
+    @Field(name = "content")
     private String content;
+
+    @Field(name = "send_time")
     private LocalDateTime sendTime;
+
+
+    @Field(name = "type")
+    private MessageType type;
 }
